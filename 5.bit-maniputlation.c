4c5f6e234fde8501b4ae15ccb8c9b51424f60d23
HughesSeanKyle <khughessean@yahoo.com>
2023-05-06 23:24:09 +0200

    Update to c

--- 5.bit-maniputlation.c
+++ 5.bit-maniputlation.c
@@ -1,34 +1,33 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
-    unsigned int num1 = 0b1010; // binary literal
-    unsigned int num2 = 0b1100; // binary literal
+    unsigned int num1 = 0xA; // binary 1010
+    unsigned int num2 = 0xC; // binary 1100
 
     // Bitwise AND
     unsigned int result1 = num1 & num2;
-    cout << "Bitwise AND: " << bitset<4>(result1) << endl; // prints 1000
+    printf("Bitwise AND: %X\n", result1); // prints 8
 
     // Bitwise OR
     unsigned int result2 = num1 | num2;
-    cout << "Bitwise OR: " << bitset<4>(result2) << endl; // prints 1110
+    printf("Bitwise OR: %X\n", result2); // prints E
 
     // Bitwise XOR
     unsigned int result3 = num1 ^ num2;
-    cout << "Bitwise XOR: " << bitset<4>(result3) << endl; // prints 0110
+    printf("Bitwise XOR: %X\n", result3); // prints 6
 
     // Bitwise NOT
     unsigned int result4 = ~num1;
-    cout << "Bitwise NOT: " << bitset<4>(result4) << endl; // prints 0101
+    printf("Bitwise NOT: %X\n", result4); // prints F5
 
     // Left Shift
     unsigned int result5 = num1 << 2;
-    cout << "Left Shift: " << bitset<4>(result5) << endl; // prints 101000
+    printf("Left Shift: %X\n", result5); // prints 28
 
     // Right Shift
     unsigned int result6 = num1 >> 1;
-    cout << "Right Shift: " << bitset<4>(result6) << endl; // prints 0101
+    printf("Right Shift: %X\n", result6); // prints 5
 
     return 0;
 }
